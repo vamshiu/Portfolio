5912b4ae2e682836e6fba330597748b73e95a91e
vamshiu <you@example.com>
2023-06-24 19:20:51 +0530

    first commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,10 +8,10 @@ function About() {
     
         <div className=' h-[800px]  bg-black flex  flex-col items-center text-white   'name='about'>
             <h1 className='font-bold text-4xl border-b-2 text-white border-[#ffff00] mt-2 mb-10  '>About Me</h1>
-            <div className='max-w-[1000px] items-center justify-center text-white grid grid-cols-1 sm:grid-cols-2 pt-10'>
+            <div className='max-w-[1100px] items-center justify-center text-white grid grid-cols-1 sm:grid-cols-2 pt-10'>
                 <img className='w-[200px] sm:w-[400px] mx-auto sm:mx-0'src={photo} alt="" />
-                <div className='p-5 ml-2 sm:ml-0'>
-                <p className='py-1'>Hi I am U Vamshi ,A passionate front end React developer based in Hyderabad, India.📍 </p>
+                <div className='p-5 ml-2 sm:ml-0 '>
+                <p className='py-1'>Hi I am U Vamshi , a passionate front end React developer based in Hyderabad, India.📍 </p>
                 <p className='py-1'>Also into problem solving using Java.</p>
                 <p className='py-1'>Qualification- Btech, VNRVJIET.</p>
                 <p className='py-1'>My hobbies include playing basketball 🏀 and photography 📷.</p>
