582538d9df869cb99337b57d897ee3e5582dec08
vamshiu <you@example.com>
2023-06-24 19:46:10 +0530

    first commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ import pin from "../assets/pin.png"
 function About() {
   return (
     
-        <div className=' h-[800px]  bg-black flex  flex-col items-center text-white   'name='about'>
+        <div className=' h-[800px]  bg-black flex  flex-col items-center text-white pt-[150px] 'name='about'>
             <h1 className='font-bold text-4xl border-b-2 text-white border-[#ffff00] mt-2 mb-10  '>About Me</h1>
             <div className='max-w-[1100px] items-center justify-center text-white grid grid-cols-1 sm:grid-cols-2 pt-10'>
                 <img className='w-[200px] sm:w-[400px] mx-auto sm:mx-0'src={photo} alt="" />
